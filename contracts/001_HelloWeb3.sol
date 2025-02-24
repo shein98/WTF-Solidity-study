fbb6933efb507ba271df1c32eee7fb8db9f35461
汤鑫 <2226092102@qq.com>
2025-02-24 10:52:54 +0800

    feat: 第一节：helloweb3

---
+++ contracts/001_HelloWeb3.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+// This contract is written in Solidity version 0.8.21
+pragma solidity ^0.8.21;
+
+// The HelloWeb3 contract
+contract HelloWeb3 {
+    // A public string variable that stores the message "Hello Web3!"
+    string public _string = "Hello Web3!";
+}
